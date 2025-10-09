5eb74d8a16f01209e83d093626b919026a5cae14
minjiwon <mjo4260@naver.com>
2025-10-09 21:30:21 +0900

    feat: 배달 접수/완료/라이더 상태 변경 기능 구현

---
+++ service/delivery/src/main/java/com/bappul/delivery/application/validator/DeliveryValidator.java
@@ -0,0 +1,27 @@
+package com.bappul.delivery.application.validator;
+
+import static com.bappul.delivery.exception.ServiceExceptionCode.NOT_FOUND_DELIVERY;
+
+import com.bappul.delivery.domain.entity.Delivery;
+import com.bappul.delivery.domain.repository.DeliveryRepository;
+import exception.ServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryValidator {
+
+  private final DeliveryRepository deliveryRepository;
+
+  public Delivery getByOrderId(Long orderId) {
+    return deliveryRepository.findByOrderId(orderId)
+        .orElseThrow(() -> new ServiceException(NOT_FOUND_DELIVERY));
+  }
+
+  public Delivery getByDeliveryId(Long deliveryId) {
+    return deliveryRepository.findById(deliveryId)
+        .orElseThrow(() -> new ServiceException(NOT_FOUND_DELIVERY));
+  }
+
+}
