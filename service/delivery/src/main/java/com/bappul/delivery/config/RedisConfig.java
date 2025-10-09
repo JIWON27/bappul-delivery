1007b60c45065f4f2bc6784ea28769ad98f92917
minjiwon <mjo4260@naver.com>
2025-10-09 21:32:50 +0900

    chore: redis 관련 설정파일 추가

---
+++ service/delivery/src/main/java/com/bappul/delivery/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.bappul.delivery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+  private final String host;
+  private final int port;
+
+  public RedisConfig(
+      @Value("${spring.data.redis.host}") String host,
+      @Value("${spring.data.redis.port}") int port
+  ) {
+    this.host = host;
+    this.port = port;
+  }
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(host, port);
+  }
+
+  @Bean
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setEnableTransactionSupport(true);
+
+    redisTemplate.setKeySerializer(new GenericToStringSerializer<>(String.class));
+    redisTemplate.setValueSerializer(new GenericToStringSerializer<>(String.class));
+
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+    return redisTemplate;
+  }
+}
