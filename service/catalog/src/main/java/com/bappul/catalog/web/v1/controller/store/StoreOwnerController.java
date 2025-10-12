d6b67eb13800b5750c65d0f23c401e92b8dc9d69
minjiwon <mjo4260@naver.com>
2025-10-13 02:37:14 +0900

    feat: 가게 대표 이미지 등록/다운로드 API 구현

--- service/catalog/src/main/java/com/bappul/catalog/web/v1/controller/store/StoreOwnerController.java
+++ service/catalog/src/main/java/com/bappul/catalog/web/v1/controller/store/StoreOwnerController.java
@@ -1,11 +1,13 @@
 package com.bappul.catalog.web.v1.controller.store;
 
 import com.bappul.catalog.application.service.MenuService;
+import com.bappul.catalog.application.service.StoreImageService;
 import com.bappul.catalog.application.service.StoreService;
 import com.bappul.catalog.web.v1.request.menu.MenuRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import response.ApiResponse;
 
 @RestController
@@ -24,6 +27,7 @@ public class StoreOwnerController {
 
   private final StoreService storeService;
   private final MenuService menuService;
+  private final StoreImageService storeImageService;
 
   @PostMapping("/{storeId}/menus")
   public ResponseEntity<ApiResponse<Void>>  enroll(
@@ -42,5 +46,14 @@ public class StoreOwnerController {
   {
     storeService.setStoreOpenStatus(storeId, status);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+
+  @PostMapping(value = "/{storeId}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<Void> updateThumbnailImage(
+      @PathVariable Long storeId,
+      @RequestParam("image") MultipartFile file)
+  {
+    storeImageService.uploadStoreImage(file, storeId);
+    return  ResponseEntity.status(HttpStatus.OK).build();
   }
 }
