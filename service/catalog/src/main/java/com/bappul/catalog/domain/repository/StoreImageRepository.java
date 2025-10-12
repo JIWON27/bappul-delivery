aa8e94d29add464e704bc5a00da5c9f8da4a3e7c
minjiwon <mjo4260@naver.com>
2025-10-13 02:36:45 +0900

    feat: 가게 대표 이미지 등록 기능 구현

--- service/catalog/src/main/java/com/bappul/catalog/domain/repository/StoreImageRepository.java
+++ service/catalog/src/main/java/com/bappul/catalog/domain/repository/StoreImageRepository.java
@@ -1,10 +1,12 @@
 package com.bappul.catalog.domain.repository;
 
+import com.bappul.catalog.domain.entity.Store;
 import com.bappul.catalog.domain.entity.StoreImage;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
-
+  Optional<StoreImage> findByStore(Store store);
 }
