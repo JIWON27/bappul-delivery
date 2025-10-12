2be57d34ab4b52d6f2d8dfcfdb0b9ba8ac33fc30
minjiwon <mjo4260@naver.com>
2025-10-13 02:09:55 +0900

    fix: StoreImageRepository의 제네릭 타입 StoreImage로 수정

--- service/catalog/src/main/java/com/bappul/catalog/domain/repository/StoreImageRepository.java
+++ service/catalog/src/main/java/com/bappul/catalog/domain/repository/StoreImageRepository.java
@@ -1,10 +1,10 @@
 package com.bappul.catalog.domain.repository;
 
-import com.bappul.catalog.domain.entity.MenuImage;
+import com.bappul.catalog.domain.entity.StoreImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StoreImageRepository extends JpaRepository<MenuImage, Long> {
+public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
 
 }
