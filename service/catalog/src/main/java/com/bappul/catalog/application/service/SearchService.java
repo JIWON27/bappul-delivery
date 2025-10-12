12c00cb47cc8a86340b2aad48574f6ae63165a51
minjiwon <mjo4260@naver.com>
2025-10-13 02:37:49 +0900

    feat: 가게 검색 시 가게 대표 이미지 조회 로직 추가

--- service/catalog/src/main/java/com/bappul/catalog/application/service/SearchService.java
+++ service/catalog/src/main/java/com/bappul/catalog/application/service/SearchService.java
@@ -1,9 +1,11 @@
 package com.bappul.catalog.application.service;
 
+import com.bappul.catalog.domain.entity.Store;
 import com.bappul.catalog.domain.repository.StoreSearchViewRepository;
 import com.bappul.catalog.domain.repository.StoreSearchViewRepository.StoreSearchResult;
 import com.bappul.catalog.web.v1.request.store.StoreSearchCondition;
 import com.bappul.catalog.web.v1.response.search.StoreSearchResponse;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,14 +17,19 @@ import response.CursorResponse;
 public class SearchService {
 
   private final StoreSearchViewRepository storeSearchViewRepository;
+  private final StoreImageService storeImageService;
 
   @Transactional(readOnly = true)
   public CursorResponse<StoreSearchResponse> search(StoreSearchCondition condition) {
     StoreSearchResult storeSearchResult = storeSearchViewRepository.findStoresWithCondition(condition);
 
-    List<StoreSearchResponse> responses = storeSearchResult.content().stream()
-        .map(StoreSearchResponse::from)
-        .toList();
+    List<Store> stores = storeSearchResult.content();
+    List<StoreSearchResponse> responses = new ArrayList<>();
+    for (Store store : stores) {
+      String imageUrl = storeImageService.getStoreImageUrl(store.getId());
+      StoreSearchResponse response = StoreSearchResponse.from(store, imageUrl);
+      responses.add(response);
+    }
 
     return CursorResponse.of(responses, storeSearchResult.nextCursor(), responses.size(), storeSearchResult.hasNext());
   }
