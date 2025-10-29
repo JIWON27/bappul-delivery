f1c614a6c2d3b13a5317026580cfa31774233faf
minjiwon <mjo4260@naver.com>
2025-10-30 04:34:57 +0900

    feat: 결제 이벤트 처리에 필요한 이벤트 타입 정의 및 보완

--- service/payment/src/main/java/com/bappul/delivery/payment/application/event/contracts/payment/PaymentSuccessEvent.java
+++ service/payment/src/main/java/com/bappul/delivery/payment/application/event/contracts/payment/PaymentSuccessEvent.java
@@ -12,5 +12,5 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PaymentSuccessEvent {
   Long orderId;
-  String merchantUid;
+  String paymentId;
 }
