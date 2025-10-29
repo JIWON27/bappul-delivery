ca78f6a2576f6a1fcb30ca70de4f60f38b901670
minjiwon <mjo4260@naver.com>
2025-10-30 05:46:16 +0900

    refactor: 주문 응답 불필요한 필드 삭제

--- service/order/src/main/java/com/bappul/order/web/v1/response/OrderResponse.java
+++ service/order/src/main/java/com/bappul/order/web/v1/response/OrderResponse.java
@@ -11,6 +11,5 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class OrderResponse {
   Long orderId;
-  String merchantUid;
   BigDecimal orderTotalPrice;
 }
