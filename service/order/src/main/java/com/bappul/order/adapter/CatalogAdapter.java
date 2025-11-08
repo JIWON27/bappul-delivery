30db98e77847723597a563f6d28c0f939293e457
minjiwon <mjo4260@naver.com>
2025-11-08 21:50:01 +0900

    feat: CatalogQoutePort → CatalogPort로 리네임 및 getLocation 추가 - 가게 위치 조회 OpenFegin Client 추가 - 위치 조회 결과를 캐싱되도록 @Cacheable 어노테이션 추가

--- service/order/src/main/java/com/bappul/order/adapter/CatalogAdapter.java
+++ service/order/src/main/java/com/bappul/order/adapter/CatalogAdapter.java
@@ -2,15 +2,19 @@ package com.bappul.order.adapter;
 
 import com.bappul.order.adapter.request.PricingInternalRequest;
 import com.bappul.order.adapter.response.PricingInternalResponse;
-import com.bappul.order.port.CatalogQuotePort;
+import com.bappul.order.adapter.response.StoreLocationResponse;
+import com.bappul.order.port.CatalogPort;
 import com.bappul.order.web.v1.request.OrderItemRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
-public class CatalogQuoteAdapter implements CatalogQuotePort {
+public class CatalogAdapter implements CatalogPort {
 
   private final CatalogClient catalogClient;
 
@@ -21,5 +25,11 @@ public class CatalogQuoteAdapter implements CatalogQuotePort {
         .items(items)
         .build();
     return catalogClient.calculate(request);
+  }
+
+  @Override
+  @Cacheable(value = "storeLocation", key = "#storeId")
+  public StoreLocationResponse getLocation(Long storeId) {
+    return catalogClient.getLocation(storeId);
   }
 }
--- service/order/src/main/java/com/bappul/order/port/CatalogPort.java
+++ service/order/src/main/java/com/bappul/order/port/CatalogPort.java
@@ -1,9 +1,11 @@
 package com.bappul.order.port;
 
 import com.bappul.order.adapter.response.PricingInternalResponse;
+import com.bappul.order.adapter.response.StoreLocationResponse;
 import com.bappul.order.web.v1.request.OrderItemRequest;
 import java.util.List;
 
-public interface CatalogQuotePort {
+public interface CatalogPort {
   PricingInternalResponse getQuote(Long storeId, List<OrderItemRequest> items);
+  StoreLocationResponse getLocation(Long storeId);
 }
