43903431f6ddcf70913030e594001366782aebae
minjiwon <mjo4260@naver.com>
2025-11-08 21:45:12 +0900

    feat: 캐싱 활성화

--- service/order/src/main/java/com/bappul/order/OrderApplication.java
+++ service/order/src/main/java/com/bappul/order/OrderApplication.java
@@ -3,11 +3,13 @@ package com.bappul.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 
+@EnableCaching
 @EnableRetry
 @EnableFeignClients
 @EnableDiscoveryClient
