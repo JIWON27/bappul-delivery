b1dd5805b43bfef7fab9fc9c6b63bd8677f8a4b2
minjiwon <mjo4260@naver.com>
2025-11-14 23:41:03 +0900

    feat: 배달 할당을 위한 조건부 업데이트 쿼리 추가

--- service/delivery/src/main/java/com/bappul/delivery/application/validator/DeliveryValidator.java
+++ service/delivery/src/main/java/com/bappul/delivery/application/validator/DeliveryValidator.java
@@ -3,6 +3,7 @@ package com.bappul.delivery.application.validator;
 import static com.bappul.delivery.exception.ServiceExceptionCode.NOT_FOUND_DELIVERY;
 
 import com.bappul.delivery.domain.entity.Delivery;
+import com.bappul.delivery.domain.entity.DeliveryStatus;
 import com.bappul.delivery.domain.repository.DeliveryRepository;
 import exception.ServiceException;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,10 @@ public class DeliveryValidator {
   public Delivery getByDeliveryId(Long deliveryId) {
     return deliveryRepository.findById(deliveryId)
         .orElseThrow(() -> new ServiceException(NOT_FOUND_DELIVERY));
+  }
+
+  public int updateDelivery(Long deliveryId, Long riderUserId) {
+    return deliveryRepository.tryAssign(deliveryId, riderUserId, DeliveryStatus.ASSIGNED);
   }
 
 }
--- service/delivery/src/main/java/com/bappul/delivery/domain/repository/DeliveryRepository.java
+++ service/delivery/src/main/java/com/bappul/delivery/domain/repository/DeliveryRepository.java
@@ -1,12 +1,24 @@
 package com.bappul.delivery.domain.repository;
 
 import com.bappul.delivery.domain.entity.Delivery;
+import com.bappul.delivery.domain.entity.DeliveryStatus;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
 
   Optional<Delivery> findByOrderId(Long orderId);
+
+  @Modifying
+  @Query("UPDATE Delivery d SET d.riderUserId = :riderId, d.status = :status WHERE d.id = :deliveryId and d.riderUserId IS NULL")
+  int tryAssign(
+      @Param("deliveryId") Long deliveryId,
+      @Param("riderId") Long riderId,
+      @Param("status") DeliveryStatus status
+  );
 }
